aee31f20a86ce783a575161b40fa9b862defefe6
Dax Buratto <daxburatto@gmail.com>
2021-02-20 17:19:07 -0600

    All tests pass! Hurray! Onto html logic

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,3 +1,13 @@
-// school
+const { test, expect } = require('@jest/globals')
+const Employee = require('../lib/Employee')
+const Intern = require('../lib/Intern')
 
-// Role Intern
+test('Creates a new Intern', async () => {
+    const intern = new Intern('Dax', 45678, 'dax@test.com', 'UTA')
+
+    expect(intern.getName()).toEqual(intern.name)
+    expect(intern.getId()).toEqual(intern.id)
+    expect(intern.getEmail()).toEqual(intern.email)
+    expect(intern.getSchool()).toEqual(intern.school)
+    expect(intern.getRole()).toEqual('Intern')
+})
